f852b1371b70fc550952610f628acbd5ea5f36ba
root <root@SiegHeart.localdomain>
2021-10-28 13:25:20 +0200

    ajout page contact, page gallery single, fix navabar

---
+++ .history/symfony/website-skeleton/src/Controller/MainController_20211026192856.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index(Request $request): Response
+    {
+        $routeName = $request->attributes->get('_route');
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+            'routeName' => $routeName,
+        ]);
+    }
+    /**
+     * @Route("/actus", name="actus")
+     */
+    public function actus(Request $request): Response
+    {
+        $routeName = $request->attributes->get('_route');
+        return $this->render('main/actus.html.twig', [
+            'controller_name' => 'MainController',
+            'routeName' => $routeName,
+        ]);
+    }
+    /**
+     * @Route("/portfolio", name="portfolio")
+     */
+    public function portfolio(Request $request): Response
+    {
+        $routeName = $request->attributes->get('_route');
+
+        return $this->render('main/portfolio.html.twig', [
+            'controller_name' => 'MainController',
+            'routeName' => $routeName,
+        ]);
+    }
+    /**
+     * @Route("/schools", name="schools")
+     */
+    public function schools(Request $request): Response
+    {
+        $routeName = $request->attributes->get('_route');
+
+        return $this->render('main/schools.html.twig', [
+            'controller_name' => 'MainController',
+            'routeName' => $routeName,
+        ]);
+    }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request): Response
+    {
+        $routeName = $request->attributes->get('_route');
+
+        return $this->render('main/contact.html.twig', [
+            'controller_name' => 'MainController',
+            'routeName' => $routeName,
+        ]);
+    }
+}
